69e0101e8d44c9fb6b832e584a0a95cdce79ec57
kokosnica <78181419+kokosnica@users.noreply.github.com>
2022-02-15 18:18:32 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+php /var/www/pterodactyl/artisan down
+cd /var/www/pterodactyl/resources/scripts
+rm -rf main.css
+echo "@import url('https://raw.githubusercontent.com/kokosnica/v2/main/user.css')" >> /var/www/pterodactyl/resources/scripts/main.css
+rm -rf index.tsx
+wget https://raw.githubusercontent.com/kokosnica/v2/main/index.tsx
+cd ../..
+php artisan view:clear
+php artisan cache:clear
+clear
+echo "User panel theme has been added."
+echo "Building panel..."
+npm install yarn -g
+yarn install
+yarn add @emotion/react
+rm -rf /var/www/pterodactyl/resources/scripts/hoc/requireServerPermission.tsx
+yarn build:production
+cd ../../..
+php /var/www/pterodactyl/artisan up
+php /var/www/pterodactyl/artisan view:clear
+php /var/www/pterodactyl/artisan cache:clear
+clear
+echo "Panel build finished. Make sure to shift + refresh your page!"
